3e435f867eb2027ee20c5940a2a3c2ba3a3f497b
Stepan Sigut <sigut.stepan@poda.cz>
2021-04-25 22:58:48 +0200

    Zmeny pro Kaju

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ __copyright__ = "Jakob Aungiers 2018"
 __version__ = "2.0.0"
 __license__ = "MIT"
 
+import numpy as np
+import pdb
 import os
 import json
 import time
@@ -21,15 +23,31 @@ def plot_results(predicted_data, true_data):
     plt.show()
 
 
-def plot_results_multiple(predicted_data, true_data, prediction_len):
+def plot_results_multiple(predicted_data, true_data, abs_data, prediction_len, prediction_interval, sequence_length):
     fig = plt.figure(facecolor='white')
-    ax = fig.add_subplot(111)
-    ax.plot(true_data, label='True Data')
-	# Pad the list of predictions to shift it in the graph to it's correct start
+    #ax = fig.add_subplot(121)
+    #ax.plot(true_data, label='True Data')
+    ax2 = fig.add_subplot(111)
+    ax2.plot(abs_data[:, 0])
+
+    #new_data = []
+    #for i in range(len(true_data)):
+    #    new_data.append((1+true_data[i]) * abs_data[i][0])
+    #ax2.plot(new_data)
+
+    # Pad the list of predictions to shift it in the graph to it's correct start
     for i, data in enumerate(predicted_data):
-        padding = [None for p in range(i * prediction_len)]
-        plt.plot(padding + data, label='Prediction')
-        plt.legend()
+        shift_width = i * prediction_interval
+        #for i in range(len(data)):
+        #ax.plot([None] * shift_width + data, label='Prediction')
+        predicted_abs = abs_data[shift_width:shift_width+prediction_len, 0] * (1 + np.array(data))
+        coef = np.vstack([np.arange(0,prediction_len), np.ones(prediction_len)]).T
+        m, c = np.linalg.lstsq(coef, predicted_abs, rcond=None)[0]
+        line_values = m * np.arange(0,prediction_len) + c
+
+        ax2.plot([None] * (shift_width + sequence_length) + list(line_values)[:10])
+        #ax2.plot([None] * (shift_width + sequence_length) + list(abs_data[shift_width, 0] * (1 + np.array(data))))
+        #plt.legend()
     plt.show()
 
 
@@ -45,21 +63,23 @@ def main():
 
     model = Model()
     model.build_model(configs)
+    #model.load_model("saved_models/AAPL-b64-e12.h5")
+    #model.load_model("saved_models/25042021-203944-e20.h5")
     x, y = data.get_train_data(
         seq_len=configs['data']['sequence_length'],
         normalise=configs['data']['normalise']
     )
 
     '''
-	# in-memory training
-	model.train(
-		x,
-		y,
-		epochs = configs['training']['epochs'],
-		batch_size = configs['training']['batch_size'],
-		save_dir = configs['model']['save_dir']
-	)
-	'''
+        # in-memory training
+        model.train(
+        	x,
+        	y,
+        	epochs = configs['training']['epochs'],
+        	batch_size = configs['training']['batch_size'],
+        	save_dir = configs['model']['save_dir']
+        )
+        '''
     # out-of memory generative training
     steps_per_epoch = math.ceil((data.len_train - configs['data']['sequence_length']) / configs['training']['batch_size'])
     model.train_generator(
@@ -79,11 +99,11 @@ def main():
         normalise=configs['data']['normalise']
     )
 
-    predictions = model.predict_sequences_multiple(x_test, configs['data']['sequence_length'], configs['data']['sequence_length'])
+    predictions = model.predict_sequences_multiple(x_test, configs['data']['sequence_length'], configs['prediction']['length'], configs['prediction']['interval'])
     # predictions = model.predict_sequence_full(x_test, configs['data']['sequence_length'])
     # predictions = model.predict_point_by_point(x_test)
 
-    plot_results_multiple(predictions, y_test, configs['data']['sequence_length'])
+    plot_results_multiple(predictions, y_test, data.data_test, configs['prediction']['length'], configs['prediction']['interval'], configs['data']['sequence_length'])
     # plot_results(predictions, y_test)
 
 
